0ef7f736df619dc5765ab280c91bb9eda3110ded
Erfan <77303475+erfan2ubc@users.noreply.github.com>
2022-10-20 20:53:12 -0700

    Updated the theme

---
+++ app/src/main/java/com/example/javabucksim/LowStackWarning.java
@@ -0,0 +1,15 @@
+package com.example.javabucksim;
+
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class LowStackWarning {
+
+
+
+
+    public void showAlert(View view) {
+//        AlertDialog.Builder alert = new AlertDialog.Builder();
+    }
+}
