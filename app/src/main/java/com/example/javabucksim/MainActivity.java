daf2a42d7d55c95182180af26fe485b4c180ebb1
Sierra Williams <williamssierra90@gmail.com>
2022-10-23 16:51:29 -0700

    updated manifest xml file to open other activities, also updated the theme xml file to remove the labels

--- app/src/main/java/com/example/javabucksim/MainActivity.java
+++ app/src/main/java/com/example/javabucksim/MainActivity.java
@@ -222,6 +222,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Categories.class);
+                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
