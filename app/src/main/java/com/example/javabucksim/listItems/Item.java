5ba1b8ceddd2a0f8acb64345423145c32092f5af
Sierra Williams <williamssierra90@gmail.com>
2022-11-14 09:59:02 -0800

    small updates, removed unused imports

--- app/src/main/java/com/example/javabucksim/listItems/Item.java
+++ app/src/main/java/com/example/javabucksim/listItems/Item.java
@@ -7,28 +7,21 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.javabucksim.R;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Item extends AppCompatActivity {
 
     TextView productName, smol, mid, chonk, one, two, three, price12, price16, price20;
     Button back;
-    String sCalories, sProtein, sFat, sCarbs, sFiber, sSugar;
-    String mCalories, mProtein, mFat, mCarbs, mFiber, mSugar;
-    String cCalories, cProtein, cFat, cCarbs, cFiber, cSugar;
-    private DatabaseReference mDatabase;
+//    String sCalories, sProtein, sFat, sCarbs, sFiber, sSugar;
+//    String mCalories, mProtein, mFat, mCarbs, mFiber, mSugar;
+//    String cCalories, cProtein, cFat, cCarbs, cFiber, cSugar;
+//    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
         back = findViewById(R.id.backButton);
         productName = findViewById(R.id.productName);
         smol = findViewById(R.id.smol);
